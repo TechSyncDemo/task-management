41a5f0cc1c772be6fc613ae76533c1ef7f6b5fa7
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2025-07-09 15:22:14 +0530

    capacitor setup done

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rptaskmanagement.com',
+  appName: 'rptaskmanagement',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
